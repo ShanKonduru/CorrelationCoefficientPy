76ca1dd4e9f305cf7bbe7decebb3a56fbd8cad06
ShanKonduru <ShanKonduru@gmail.com>
2024-04-25 21:49:26 +0530

    half & full correlation coefficient graph

--- 003_run.bat
+++ 003_run.bat
@@ -1,2 +1,16 @@
 @echo off
+@if '%1'=='FULL' goto FULL
+@if '%1'=='HALF' goto HALF
+goto END
+
+
+:FULL
 poetry run python winequality.py
+goto END
+
+:HALF
+poetry run python halfwinequality.py
+goto END
+
+
+:END
--- halfwinequality.py
+++ halfwinequality.py
@@ -1,6 +1,7 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 
 # Load wine quality dataset from CSV file
 wine_df = pd.read_csv('winequality-white.csv', delimiter=';')
@@ -8,9 +9,13 @@ wine_df = pd.read_csv('winequality-white.csv', delimiter=';')
 # Calculate correlation coefficients
 correlation_matrix = wine_df.corr()
 
+# Mask upper triangle of correlation matrix
+mask = np.triu(np.ones_like(correlation_matrix, dtype=bool))
+
 # Plot correlation matrix as heatmap
 sns.set(font_scale=1.2)
 plt.figure(figsize=(10, 8))
-sns.heatmap(correlation_matrix, square=True, cmap='bwr', annot=True, fmt=".2f", vmin=-1, vmax=1, linewidth=0.5)
-plt.title('Correlation Coefficient Heatmap for Wine Quality Dataset')
+sns.heatmap(correlation_matrix, mask=mask, square=True, cmap='bwr', annot=True, fmt=".2f", vmin=-1, vmax=1, linewidth=0.5)
+
+plt.title('Correlation Coefficient Heatmap for Wine Quality Dataset (Lower Triangle)')
 plt.show()
